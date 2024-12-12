72af4a6d7c9100eb8aa212e97a5b6769ce69c825
Mat15 <mathias.benoit@efrei.net>
2024-12-11 19:32:56 -0500

    interface update

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from train_model import train_model
 import mlflow
 from threading import Thread
 import os
+import base64
 
 app = Flask(__name__)    
 
@@ -18,18 +19,16 @@ def home():
         if file:
             image_bytes = file.read()
             predicted_class, probability = predict_image(image_bytes)
-            return redirect(url_for('detection', predicted_class=predicted_class, probability=probability))
+            encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+            return redirect(url_for('detection', predicted_class=predicted_class, probability=probability, image_data=encoded_image))
     return render_template("index.html")
 
 @app.route('/detection')
 def detection():
     predicted_class = request.args.get('predicted_class')
     probability = request.args.get('probability')
-    return render_template("detection.html", predicted_class=predicted_class, probability=probability)
+    image_data = request.args.get('image_data')
+    return render_template("detection.html", predicted_class=predicted_class, probability=probability, image_data=image_data)
 
 if __name__ == '__main__':
-    if os.getenv("RUNNING_IN_DOCKER"):
-        mlflow.set_tracking_uri("http://mlflow:5000")  # For Docker tracking server
-    else:
-        mlflow.set_tracking_uri("http://localhost:5001")  # For local tracking server
     app.run(debug=False, host='0.0.0.0', port=5000)
